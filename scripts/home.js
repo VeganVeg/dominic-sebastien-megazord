ab67f673a26b6d9672c83f4c99d043579ff81453
VeganVeg <dominicyale.03@gmail.com>
2022-04-13 17:52:30 -0400

    p

--- scripts/home.js
+++ scripts/home.js
@@ -1,12 +1,23 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to('.section', {
-  scrollTrigger: {
-    markers: true,
-    trigger: '.section',
-  },
-  x: '40%',
-  duration: 1,
-});
+let baniereArr = document.querySelectorAll('body');
 
-console.log("grasdur");
+baniereArr.forEach(function(book) {
+  let imagesArr = document.querySelectorAll('section');
+    imagesArr.forEach(function(img) {
+    console.log(img)
+    gsap.fromTo(img, {
+    scrollTrigger: {
+      markers: true,
+      start: ' center 50%',
+      end: ' 30%',
+      trigger: img,
+      toggleActions: 'restart complete reverse reset',
+      x: '-25%'
+    },
+    x: '10%',
+    duration: 0.08,
+      });
+    });
+  
+});
