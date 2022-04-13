626e6c12e89ad58c03ddcd18d16fcc3b1d231e39
VeganVeg <dominicyale.03@gmail.com>
2022-04-13 18:20:35 -0400

    p

--- scripts/home.js
+++ scripts/home.js
@@ -5,18 +5,17 @@ let baniereArr = document.querySelectorAll('body');
 baniereArr.forEach(function(book) {
   let imagesArr = document.querySelectorAll('section');
     imagesArr.forEach(function(img) {
-    console.log(img)
-    gsap.fromTo(img, {
+    gsap.to(img, {
     scrollTrigger: {
-      markers: true,
-      start: ' center 50%',
-      end: ' 30%',
+      markers: false,
+      start: 'top 70%',
+      end: 'tpo 50%',
       trigger: img,
       toggleActions: 'restart complete reverse reset',
-      x: '-25%'
     },
-    x: '10%',
-    duration: 0.08,
+    y: '-30px',
+    opacity: '1',
+    duration: 0.3,
       });
     });
   
