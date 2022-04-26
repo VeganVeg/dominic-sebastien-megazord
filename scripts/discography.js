41a3dbaf11220d17f7851d85ac832572ef3c81a4
SabrinaLaforest <70386265+SabrinaLaforest@users.noreply.github.com>
2022-04-26 09:50:32 -0400

    paroles

--- scripts/discography.js
+++ scripts/discography.js
@@ -11,3 +11,21 @@ gsap.timeline({
 
 .to('section', {rotation: 360, opacity: 0,})
 .to('h2', {rotation: 360, opacity: 0,});
+
+
+  
+  let titre = document.querySelector('.titre');
+  let submit = document.querySelector('.submit');
+  
+  submit.addEventListener('click', function () {
+    else.preventDefault();
+    if (titre != null){
+        fetch("https://api.lyrics.ovh/v1/redhotchilipepper/"submit.value)
+            .then(data => data.json())
+            .then(paroles => {
+                const newLineToBr = function (str) {
+                    return str.replace(/(?:\r\n|\r|\n)/g, '<br>');
+  }
+            });
+    }
+  })
