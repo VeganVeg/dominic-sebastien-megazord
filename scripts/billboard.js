c94cf283cafd4f83d9d6e3a03a075b4ea12185d3
SabrinaLaforest <70386265+SabrinaLaforest@users.noreply.github.com>
2022-05-04 17:56:06 -0400

    paroles

--- scripts/billboard.js
+++ scripts/billboard.js
@@ -1,8 +1,4 @@
 
-/* Bonjour, apparement on dirait que ma page js ne veut pas se connecter a la page html;
- le gsap fonctionne bien je l'ai fait avec codepen svp pourriez vous regarder ce qui ne 
-fonctionne pas on(sebastien et moi) ne comprend pas */
-
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.timeline({
@@ -13,68 +9,71 @@ gsap.timeline({
     trigger: '.titre',
   },
 })
-  .from('.cOne', { x: '100%', duration: 1, })
-  .from('.anim', { opacity: 0, duration: 1, })
-  .from('.cTwo', { x: '100%', duration: 1, })
-  .from('.cThree', { x: '100%', duration: 1, })
-  .from('.cFour', { x: '100%', duration: 1, })
-  .from('.cFive', { x: '100%', duration: 1, })
-  .from('.cSix', { x: '100%', duration: 1, })
-  .from('.video', { opacity: 0, duration: 1, })
-  .from('.mySwiper', { opacity: 0, duration: 1, })
+.from('.cOne', { x: '100%', duration: 1, })
+.from('.anim', { opacity: 0, duration: 1, })
+.from('.cTwo', { x: '100%', duration: 1, })
+.from('.cThree', { x: '100%', duration: 1, })
+.from('.cFour', { x: '100%', duration: 1, })
+.from('.cFive', { x: '100%', duration: 1, })
+.from('.cSix', { x: '100%', duration: 1, })
+.from('.video', { opacity: 0, duration: 1, })
+.from('.mySwiper', { opacity: 0, duration: 1, })
 
-  const anim = document.querySelector(".anim");
+const anim = document.querySelector(".anim");
 
 gsap.to('.anim', {
   scrollTrigger: {
     trigger: '.title-1',
     start: 'top 0',
-    end: 'bottom 100%', 
+    end: 'bottom 100%',
     
     onEnter: () => {
-      body.classList.add('pepperAnim2');
-      body.classList.remove('pepperAnim3');
-      body.classList.remove('pepperAnimIdle');
+      anim.classList.add('pepperAnim2');
+      anim.classList.remove('pepperAnim3');
+      anim.classList.remove('pepperAnimIdle');
     },
     onUpdate: () => {
-      body.classList.add('pepperIdle');
-      body.classList.remove('pepperAnim3');
-      body.classList.remove('pepperAnim2');
+      anim.classList.add('pepperIdle');
+      anim.classList.remove('pepperAnim3');
+      anim.classList.remove('pepperAnim2');
     },
     onLeave: () => {
-      body.classList.add('pepper3');
-      body.classList.remove('pepperAnimIdle');
-      body.classList.remove('pepperAnim2');
+      anim.classList.add('pepper3');
+      anim.classList.remove('pepperAnimIdle');
+      anim.classList.remove('pepperAnim2');
     },
-  }});   
+  }
+});
 
-  let titre = document.querySelector('.titreParole');
-  //let submit = document.querySelector('.submit');
-  let form = document.getElementById('form');
-  const groupe = "red hot chili peppers";
-  const divParole = document.querySelector('.parole');
-  
-  form.addEventListener('submit', function (e) {
-    console.log(titre.value);
-    e.preventDefault();
-    if (titre != null){
-        fetch("https://api.lyrics.ovh/v1/" + groupe + "/" + titre.value)
-            .then(data => data.json())
-            .then(paroles => {
-              newLineToBr(paroles.lyrics);
-              //console.log(paroles.lyrics);
-            })
-          }
-    /* function createHtml(sab){
-    
-     
-    } */
-      const newLineToBr = function (str) {
-        //console.log(str);
-                  //str.replace(/(?:\r\n|\r|\n)/g, '<br>');
-                  console.log(str); 
-                  let html = `<p>${str.replace(/(?:\r\n|\r|\n)/g, '<br>')}</p>`;
-                  divParole.innerHTML = html;}
+let titre = document.querySelector('.titreParole');
+let form = document.getElementById('form');
+const groupe = "red hot chili peppers";
+const divParole = document.querySelector('.parole');
+
+form.addEventListener('submit', function (e) {
+  //loading 
+  let loading = `<div class="d-flex justify-content-center">
+  <div class="spinner-border" role="status">
+  <span class="visually-hidden">Loading...</span>
+  </div>
+  </div>`;
+  //paroles
+  e.preventDefault();
+    divParole.innerHTML = loading;
+    fetch("https://api.lyrics.ovh/v1/" + groupe + "/" + titre.value)
+    .then(data => data.json())
+    .then(paroles => {
+      newLineToBr(paroles.lyrics);
     })
+    .catch(error => {
+      divParole.innerHTML = `Désolé, les paroles n'ont pu être trouvées. En voici la raison: ${error}`;
+    }
+    )
   
- 
+  
+  const newLineToBr = function (str) {
+    let html = `<p>${str.replace(/(?:\r\n|\r|\n)/g, '<br>')}</p>`;
+    divParole.innerHTML = html;
+  }
+});
+
